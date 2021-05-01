602e55a1c0444a7634fc27cdd068bc29f5fe7017
AhmadBilal07 <ahmadbilal07@live.com>
2021-05-01 14:21:36 +0200

    Flight Data Fetcher Class Created

--- src/flightdatafetcher.cpp
+++ src/flightdatafetcher.cpp
@@ -2,15 +2,19 @@
 #include <QDebug>
 
 FlightDataFetcher::FlightDataFetcher(QObject *parent):
+
 QObject(parent)
 {
+
 }
 void FlightDataFetcher::printMessage(QString txt)
 {
-qDebug() << "Message from QML: " << txt;
+    qDebug() << "Message from QML: " << txt;
 }
-void FlightDataFetcher::callApi()
-{
-qDebug() << "Inside API Caller Function" ;
+void FlightDataFetcher::callApi() {
+
+    qDebug() << "Inside API Caller Function" ;
+    QNetworkRequest request(QUrl("https://608acc18737e470017b73ee1.mockapi.io/users"));
+    NetRepl = NetAccMan.get(request);
 
 }
